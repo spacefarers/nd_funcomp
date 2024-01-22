838e70c3d16b4fe2575981f45fb6623bfd1346e4
spacefarers <michael@yangm.tech>
2024-01-22 15:48:58 -0500

    upd

---
+++ lab1/prog1.c
@@ -0,0 +1,21 @@
+//
+// Created by Michael Yang on 1/22/24.
+//
+
+#include <stdio.h>
+
+int main() {
+    int touchdowns, extra_points, field_goals, safeties;
+    printf("Enter the number of touchdowns: ");
+    scanf("%d", &touchdowns);
+    printf("Enter the number of extra points: ");
+    scanf("%d", &extra_points);
+    printf("Enter the number of field goals: ");
+    scanf("%d", &field_goals);
+    printf("Enter the number of safeties: ");
+    scanf("%d", &safeties);
+// calculate total score
+    int total_score = touchdowns * 6 + extra_points + field_goals * 3 + safeties * 2;
+    printf("The total score is %d\n", total_score);
+    return 0;
+}
---
+++ lab1/prog2.c
@@ -0,0 +1,10 @@
+//
+// Created by Michael Yang on 1/22/24.
+//
+
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+
+}
